f57225c8c17175c9046a5919181aac4a5c55d61c
paras-aghija <parasaghija2000@gmail.com>
2021-08-19 04:59:00 +0530

    darkMode + tcolorBug

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,14 +2,28 @@ import './Layout.css'
 import Sidebar from '../sidebar/Sidebar'
 import TopNav from '../topnav/TopNav'
 import Routes from '../Routes'
-
+import { useSelector, useDispatch } from 'react-redux'
+import ThemeAction from '../../redux/actions/ThemeAction'
+import { useEffect } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 
 const Layout = () => {
+    const themeReducer = useSelector(state => state.ThemeReducer)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        const themeClass = localStorage.getItem('themeMode','theme-mode-light')
+        const colorClass = localStorage.getItem('themeColor','theme-mode-light')
+
+        dispatch(ThemeAction.setMode(themeClass))
+        dispatch(ThemeAction.setColor(colorClass))
+
+    }, [dispatch])
+
     return (
         <BrowserRouter>
             <Route render={(props) => (
-                <div className='layout'>
+                <div className={`layout ${themeReducer.mode} ${themeReducer.color}`}>
                     <Sidebar {...props}/>
                     <div className="layout__content">
                         <TopNav/>
--- src/index.js
+++ src/index.js
@@ -5,12 +5,19 @@ import "./assets/css/theme.css";
 import "./assets/boxicons-2.0.7/css/boxicons.min.css";
 import "./assets/css/grid.css";
 import Layout from "./components/layout/Layout";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./redux/reducers";
+
+const store = createStore(rootReducer);
 
 document.title = "CRM";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Layout />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <Layout />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById("root")
 );
