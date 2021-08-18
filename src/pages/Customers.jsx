e241edc1702d95bfbf6bd2e6a50127d1f4bda3db
paras-aghija <parasaghija2000@gmail.com>
2021-08-18 22:14:26 +0530

    customer-cmplt

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,7 +1,51 @@
+import customerList from '../assets/JsonData/customers-list.json'
+import Table from '../components/table/Table'
+
+const customerTableHead = [
+        "id",
+        "name",
+        "email",
+        "location",
+        "phone",
+        "total_spend",
+        "total_orders"
+]
+
+const renderHead = (item, index) => <th key={index}>{item}</th>
+
+const renderBody = (item, index) => (
+    <tr key={index}>
+        <td>{item.id}</td>
+        <td>{item.name}</td>
+        <td>{item.email}</td>
+        <td>{item.location}</td>
+        <td>{item.phone}</td>
+        <td>{item.total_spend}</td>
+        <td>{item.total_orders}</td>
+    </tr>
+)
+
 const Customers = () => {
     return (
         <div>
-            Customers
+            <h2 className="page-header">
+                customers
+            </h2>
+            <div className="row">
+                <div className="col-12">
+                    <div className="card">
+                        <div className="card__body">
+                            <Table
+                                    limit='10 '
+                                    headData={customerTableHead}
+                                    renderHead = { (item,index) => renderHead(item,index)}
+                                    bodyData={customerList}
+                                    renderBody = { (item,index) => renderBody(item,index)}
+                                />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
